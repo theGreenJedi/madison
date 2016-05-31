5704b93fd3e4f7a093e72d7203a5099d4a4f6115
Tanner Doshier <git@doshitan.com>
2016-05-31 15:02:11 -0500

    Unify Annotations, Annotation Comments, and Comments
    - Model all 'Annotations', 'Annotation Comments' and 'Comments' as
      Annotations on the backend. Likes/Flags/Tags/Seen indicators/Ranges
      are also modeled as Annotations.
    - Comments on a certain part of the document are now called 'Notes'
      instead of 'Annotations' in non-user-visible stuff, it's not a
      complete change of terminology yet, but a start
    - 'Notes' are just considered a special case of 'Comments' in the API,
      all access to either flows through the `/api/doc/:id/comments/` routes
    - Fix flagging of comments (Issue #771)
    - Remove remaining independent sponsor stuff
    - Remove `/api/docs/:id/activity` route and supporting methods since
      it's purpose is achieved through the `/api/docs/:id/comments` route
    - Restores some accidentally deleted document routes, they got lost in
      the project restructure
    - Fix getting deleted docs
    - Allow use of either id or slug to bind doc model, have some special
      routes that required a slug and all others taking only an id is silly,
      accept both as identifiers for now
    - Move document access middlewares to form request validators, makes it
      much more explicit in the controller method for what permission checks
      have been done already or might still be needed
    - Add more route model bindings (e.g., {doc}) and adjust controller
      methods to use it
    - Remove Elastic Search stuff
    - Fix database clear command
    - Tweak makefile clean target
    - Fix RSS feeds, these have just been broken for a while, at least since
      the Laravel 5 upgrade
    - Remove ModalController and related things, unused
    - Cleanup some docs
    - Remove server/blueprint/routes.md
    - Remove dead routes, like the sitemap
    
    TODO: open issue about sitemap
    TODO: can we switch 'Annotation' to 'Note' in the UI?
    TODO: test restoring a deleted doc

--- server/app/Events/FeedbackSeen.php
+++ server/app/Events/FeedbackSeen.php
@@ -4,6 +4,7 @@ namespace App\Events;
 
 use App\Events\Event;
 use App\Models\User;
+use App\Models\Annotation;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
@@ -19,9 +20,9 @@ class FeedbackSeen extends Event
      *
      * @return void
      */
-    public function __construct($feedback, User $user)
+    public function __construct(Annotation $feedback, User $user)
     {
-        $this->feedback = $feedback; // either an Annotation or Comment
+        $this->feedback = $feedback;
         $this->user = $user;
     }
 
@@ -42,7 +43,7 @@ class FeedbackSeen extends Event
 
     public static function getDescription()
     {
-        return 'When feedback (an annotation or comment) of yours is seen by the document sponsor';
+        return 'When feedback (a note or comment) of yours is seen by a document sponsor';
     }
 
     public static function getType()
